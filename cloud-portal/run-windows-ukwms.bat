0a32050a5cedbb6d0171f9c4327b5b3b15523367
almondaja <102125154+almondaja@users.noreply.github.com>
2026-06-27 12:50:20 +0700

    Delete cloud-portal directory

--- cloud-portal/run-windows-ukwms.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set MQTT_URL=mqtt://192.168.0.115:1883
-set MQTT_USER=ukwms
-set MQTT_PASSWORD=ukwms
-set PORT=3000
-npm install
-npm start
-pause
--- cloud-portal/run-windows-ukwms.ps1
+++
@@ -1,6 +0,0 @@
-$env:MQTT_URL="mqtt://192.168.0.115:1883"
-$env:MQTT_USER="ukwms"
-$env:MQTT_PASSWORD="ukwms"
-$env:PORT="3000"
-npm install
-npm start
